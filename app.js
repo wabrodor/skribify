7a31828efdf963f6e9fd87494e45ad6f0497ac0d
unknown <wabrodor@gmail.com>
2021-06-30 12:48:39 +0300

    first section finished

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 AOS.init();
 
+// variables
 
+const mobileMenu = document.querySelector(".nav-links")
+const toggle = document.querySelector(".toggle")
+const fixed = document.querySelector(".fixed")
+
+
+// events
+
+toggle.addEventListener("click", ()=>{
+  mobileMenu.classList.toggle("list-active")
+})
+
+window.addEventListener("scroll",() =>{
+
+// console.log(getBoundingClientReact())
+})
 // preloader
 
 const preloader = document.querySelector(".preloader");
